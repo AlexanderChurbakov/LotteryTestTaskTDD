79110366a82be16658bdafacb43aabcc3b97525d
sasha <lalkaedheraerh1@mail.ru>
2022-12-29 10:24:39 +0300

    Initial commit

---
+++ src/main/kotlin/LotteryValidation.kt
@@ -0,0 +1,12 @@
+interface LotteryValidation {
+    fun valid(number: Int)
+
+    class Base(): LotteryValidation {
+        override fun valid(number: Int) {
+            TODO("Not yet implemented")
+        }
+
+
+    }
+
+}
