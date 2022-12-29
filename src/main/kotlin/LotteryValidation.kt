b89f15548be0a3ea5a36442c278062f033c43a6f
sasha <lalkaedheraerh1@mail.ru>
2022-12-29 11:05:59 +0300

    tests and realization need to fix realization

--- src/main/kotlin/LotteryValidation.kt
+++ src/main/kotlin/LotteryValidation.kt
@@ -1,12 +1,29 @@
+/*
+вход:
+    Функция должна принимать число количество цифр которого от 2 до 8\
+условия:
+    Выигрышный билет - сумма левой половины номера равна сумме правой половины
+результат:
+    Тру если победа фолс если поражение
+ */
 interface LotteryValidation {
-    fun valid(number: Int)
 
-    class Base(): LotteryValidation {
-        override fun valid(number: Int) {
-            TODO("Not yet implemented")
+    fun valid(number: Long): Boolean
+
+    class Base() : LotteryValidation {
+        override fun valid(number: Long): Boolean {
+            val numberString = number.toString()
+            val length = numberString.length
+            if (length == 1 || length == 9) {
+                throw IllegalArgumentException("number must be from 2 to 8 length")
+            }
+            val list = mutableListOf<Int>(length)
+            var sum = 0
+            for (num in numberString) {
+                list.add(num.toString().toInt())
+            }
+            // TODO: do algorithm
+
         }
-
-
     }
-
 }
